fc98a54904f22bdb2b6ef6a9a76c26219f867233
Jared Coleman <jaredcoleman916@gmail.com>
2018-02-07 11:46:24 -0800

    Adds dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 echo "Assumes all libraries that come with the L4T 28.1 jetpack are installed"
 read -p "Press enter to continue"
 
-sudo apt-get install libgsl-dev
+sudo apt-get install libgsl-dev libconfig++8-dev
 
