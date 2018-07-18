a81a32e06d8e1b9549b61604c0a30c545f0159c4
Jared Coleman <jaredraycoleman@gmail.com>
2018-07-17 21:48:04 -0700

    updates install dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 read -p "Various packages will be installed to your system. Press enter to continue."
 
-sudo apt-get install libgsl-dev libconfig++8-dev cmake libboost-all-dev libopencv-dev python-opencv
+sudo apt-get install libgsl-dev libconfig++-dev cmake libboost-all-dev libopencv-dev python-opencv
 
 
